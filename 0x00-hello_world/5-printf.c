eeb22dd9a8b69a711171109236b74c216b4f74a5
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-16 09:36:59 +0200

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: print a simple statement using printf function
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
