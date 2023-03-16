191a4cf32bd8dd88bb38b99ac519b564f5d8cd73
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-16 09:00:20 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main ()
+/*
+ * simple file to print a simple statement
+ */
+
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n")
-    return 0
+puts("Programming is like building a multilingual puzzle\n")
+return 0
 }
