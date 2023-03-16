ce41b9a4c88d4cc18dcfafa21c8f2e2b0b8bd344
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-16 10:25:51 +0200

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,14 @@ int main(void)
 char c;
 int i;
 long int li;
-long long ll;
+long long int lli;
 float f;
 
-printf("Size of a char is: %2d byte(s)\n", sizeof(c));
-printf("Size of an int is: %2d byte(s)\n", sizeof(i));
-printf("Size of a long int is: %2d byte(s)\n", sizeof(li));
-printf("Size of a long long is: %2d byte(s)\n", sizeof(ll));
-printf("Size of a float is: %2d byte(s)\n", sizeof(f));
+printf("Size of a char: %2d byte(s)\n", sizeof(c));
+printf("Size of an int: %2d byte(s)\n", sizeof(i));
+printf("Size of a long int: %2d byte(s)\n", sizeof(li));
+printf("Size of a long long: %2d byte(s)\n", sizeof(lli));
+printf("Size of a float: %2d byte(s)\n", sizeof(f));
 
 return (0);
 }
