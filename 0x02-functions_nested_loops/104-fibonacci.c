09cdea7553c583404abd1b68bada9089068de104
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-21 19:29:15 +0200

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,15 +7,25 @@
 
 int main(void)
 {
-	unsigned long first = 0;
-	unsigned long second = 1;
-	unsigned long result;
+	unsigned long int first = 0;
+	unsigned long int second = 1;
+	unsigned long int result;
+	unsigned long int breakpoint = 10000000000;
+	unsigned long int extra;
+	unsigned long int end;
 	int i;
 
 	for (i = 0; i < 98; i++)
 	{
 		result = first + second;
-		printf("%lu", result);
+		if (result < breakpoint)
+			printf("%lu", result);
+		else
+		{
+			extra = result / breakpoint;
+			end = result % breakpoint;
+			printf("%lu%010lu", extra, end);
+		}
 
 		if (i < 97)
 			printf(", ");
