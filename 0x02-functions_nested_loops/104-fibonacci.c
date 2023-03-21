1183a72c22088c9e380376f327922d9f69bbb5c2
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-21 20:51:09 +0200

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,11 @@
 
 int main(void)
 {
-	unsigned long int first = 0;
-	unsigned long int second = 1;
-	unsigned long int result;
-	unsigned long int breakpoint = 10000000000;
-	unsigned long int divid1;
-	unsigned long int divid2;
-	unsigned long int module1;
-	unsigned long int module2;
-	unsigned long int half1;
-	unsigned long int half2;
+	unsigned long int first = 0, second = 1, result, breakpoint = 10000000000;
+	unsigned long int divid1, divid2, module1, module2, half1, half2;
 	int i;
 
-	for (i = 0; i < 93; i++)
+	for (i = 0; i < 92; i++)
 	{
 		result = first + second;
 		printf("%lu", result);
@@ -27,12 +19,12 @@ int main(void)
 		first = second;
 		second = result;
 	}
+	divid1 = first / breakpoint;
+	divid2 = second / breakpoint;
+	module1 = first % breakpoint;
+	module2 = second % breakpoint;
 	for (i = 93; i < 99; i++)
 	{
-		divid1 = first / breakpoint;
-		divid2 = second / breakpoint;
-		module1 = first % breakpoint;
-		module2 = second % breakpoint;
 		half1 = divid1 + divid2;
 		half2 = module1 + module2;
 		if (module1 + module2 > 9999999999)
@@ -43,12 +35,11 @@ int main(void)
 		printf("%lu%lu", half1, half2);
 		if (i < 98)
 			printf(", ");
-		else
-			printf("\n");
 		divid1 = divid2;
 		divid2 = half1;
 		module1 = module2;
 		module2 = half2;
 	}
+	printf("\n");
 	return (0);
 }
