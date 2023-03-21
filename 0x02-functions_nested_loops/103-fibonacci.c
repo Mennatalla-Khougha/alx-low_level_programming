d0ba984a4ff0967a99b72e3dc3e5914b028c4158
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-21 18:27:03 +0200

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,15 +11,15 @@ int main(void)
 	long second = 1;
 	long result = 0;
 	int i;
-    long sum = 0;
+	long sum = 0;
 
 	for (i = 0; result < 4000000; i++)
 	{
 		result = first + second;
-        if (result % 2 == 0)
-        {
-		    sum += result;
-        } 
+		if (result % 2 == 0)
+		{
+			sum += result;
+		}
 		first = second;
 		second = result;
 	}
