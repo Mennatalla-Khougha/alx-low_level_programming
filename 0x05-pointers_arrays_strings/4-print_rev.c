2915f1a2e9239e29a64c2ebdd1f8e7d3ecf19705
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-27 13:03:58 +0200

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,17 @@ void print_rev(char *s)
 	int i = 0;
 	int j;
 
-	while (s[i] != 0)
+	while (*s != 0)
 	{
 		i++;
+		s++;
 	}
+	s--;
 
 	for (j = i; j >= 0; j--)
 	{
-		_putchar(s[j]);
+		_putchar(*s);
+		s--;
 	}
+	_putchar('\n');
 }
