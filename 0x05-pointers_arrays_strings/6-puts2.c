b58fb0e7b0c8dfd9e4b64cb4bcea5a8ea5d3dcc4
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-27 13:35:38 +0200

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character.
+ * @str: string.
+ * Return: nothing.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != 0)
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j+=2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
