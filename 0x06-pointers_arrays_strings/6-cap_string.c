67c7f62d99ee09017f7636bd18e72de58909ea3a
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-29 17:56:03 +0200

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of string.
+ * @str: string.
+ * Return: str.
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		switch (str[i - 1])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (str[i] > 96 && str[i] < 123)
+					str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
