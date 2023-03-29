7e6d7f0d1b16109ea116d0bdf8709ad8ddeea966
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-29 18:15:23 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,9 +9,20 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	char cap[] = ",;.!?(){}\n\t\" ";
+	int j;
+	int n;
+
+	while (cap[j] !=0)
+		j++;
 
 	while (str[i] != 0)
 	{
+		for (n = 0; n < j; n++)
+		{
+			if (str[i - 1] == cap[j])
+			{
+				/*
 		switch (str[i - 1])
 		{
 			case ' ':
@@ -26,9 +37,10 @@ char *cap_string(char *str)
 			case '(':
 			case ')':
 			case '{':
-			case '}':
+			case '}':*/
 				if (str[i] > 96 && str[i] < 123)
 					str[i] -= 32;
+			}
 		}
 		i++;
 	}
