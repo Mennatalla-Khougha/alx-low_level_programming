1f82848b5ecc1d9380baf527d4c429b626289602
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-30 00:42:55 +0200

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - print integers
+ *@n: integer.
+ * Return: nothing.
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		x = n;
+		_putchar('-');
+	}
+
+	x /= 10;
+	if (x != 0)
+		print_number(x);
+	_putchar('0' + (unsigned int) n % 10);
+}
