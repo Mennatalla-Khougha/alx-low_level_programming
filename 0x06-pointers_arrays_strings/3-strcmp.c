2557d45715035e0a0b1ca6779018c18643f679b7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-03-29 17:17:19 +0200

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,30 +4,18 @@
  *_strcmp - compares two strings.
  *@s1: string to be compared with.
  *@s2: string to be compared.
- * Return: 0 if s1 and s2 are equals, -value if s1 is less than s2, +value if s1 is greater then s2.
+ * Return: 0 s1, s2 equals, -value s1 less s2, +value s1 is more s2.
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int sum1 = 0;
-	int j = 0;
-	int sum2 = 0;
 
-	while (s1[i] != 0)
+	while (s1[i] != 0 || s2[i] != 0)
 	{
-		sum1 += s1[i];
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-
-	while (s2[j] != 0)
-	{
-		sum2 += s2[j];
-		j++;
-	}
-
-	if (i == j && sum1 == sum2)
-		return (0);
-	else
-		return (sum1 - sum2);
+	return (0);
 }
