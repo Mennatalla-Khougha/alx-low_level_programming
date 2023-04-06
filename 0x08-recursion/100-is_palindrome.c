51861bf84013da62eaa876632fb043799ba4a8b6
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-06 02:44:50 +0200

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,7 @@
 
 int is_palindrome(char *s);
 int _strlen_recursion(char *s);
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2, int len1, int len2);
 void rev(char *s, int length, char *x);
 
 /**
@@ -32,21 +32,23 @@ int _strlen_recursion(char *s)
  * _strcmp - compare two string.
  * @s1: string.
  * @s2: string.
+ * @len1:int.
+ * @len2: int.
  * Return: o the same. any other value differant.
  */
 
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2, int len1, int len2)
 {
-	if (*s1 != 0 || *s2 != 0)
+	if (s1[len1] != 0 || s2[len2] != 0)
 	{
-		if (*s1 != *s2)
+		if (s1[len1] != s2[len2])
 			return (1);
 	}
-	if (*s1 == 0 || *s2 == 0)
+	if (s1[len1] == 0 || s2[len2] == 0)
 		return (0);
-	*s1++;
-	*s2++;
-	_strcmp(s1, s2);
+	len1++;
+	len2++;
+	_strcmp(s1, s2, len1, len2);
 	return (0);
 }
 
@@ -82,10 +84,10 @@ void rev(char *s, int length, char *x)
 int is_palindrome(char *s)
 {
 	char x[100];
+	int y;
 
 	rev(s, _strlen_recursion(s), x);
-	int y;
-       	y = _strcmp(s, x);
+       	y = _strcmp(s, x, 0, 0);
 
 	if (y == 0)
 		return (1);
