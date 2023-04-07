49c6f1090ad422cfb451b5441c1254f5297376dd
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-07 21:00:19 +0200

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: number of arguments.
+ * @argv: array of elements.
+ * Return: 0 success, 1 fail.
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+       	int result = 0;
+	int j;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1, i < argc; i++)
+	{
+		for (j = 0; j < strlen(argv[i]; j++))
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		result += argv[i];
+	}
+	
+	printf("%d\n", result);
+	return (0);
+}
