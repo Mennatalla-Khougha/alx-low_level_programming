17775ae0eadaea67aba039b91c553d68316279bb
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-11 20:06:42 +0200

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,12 +17,18 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	array = malloc(sizeof(int *) * height);
 	if (array == NULL)
+	{
+		free(array);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			free(array[i]);
 			return (NULL);
+		}
 	}
 	return (array);
 }
