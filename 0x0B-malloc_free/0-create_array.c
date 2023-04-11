e7fd5f2ca525fc98691787879ba0090765f724ec
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-11 11:53:41 +0200

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, initialize it.
+ * @size: size of the array.
+ * @c: char to initialize the array.
+ * Return: NULL, or pointer to the array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *t;
+
+	t = malloc(sizeof(char) * size);
+	if (t == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		t[i] = c;
+	}
+
+	return (t);
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+We learn about malloc and free, We learn dynamic memory allocation by solving 5 mandatory tasks and 2 advanced tasks
