e45dc452fd3a2650da368a9e3701ceeb942b80b9
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-12 02:10:35 +0200

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -32,7 +32,7 @@ int words(char *str)
 
 	for (i = 0; i < strlen(str); i++)
 	{
-		if (str[i] == '' && (i != 0 && str[i + 1] != '')
+		if (str[i] == ' ' && (i != 0 && str[i + 1] != ' ')
 		{
 			word++;
 			i += len(str + i);
