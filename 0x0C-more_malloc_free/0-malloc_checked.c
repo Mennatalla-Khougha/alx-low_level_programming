20126aa811691f983c6fcff514da744f6d7bc65f
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-13 11:50:11 +0200

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,9 +9,9 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	unsigned int *p;
 
-	p = malloc(sizeof(unsigned int) * b);
+	p = (unsigned int *) malloc(sizeof(unsigned int) * b);
 
 	if (p == NULL)
 		exit(98);
