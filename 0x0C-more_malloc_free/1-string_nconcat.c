7d02db592fb93f516634c3d28902ebdd775d3b03
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-13 12:33:16 +0200

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates 2 strings.
+ * @s1: string.
+ * @s2: string.
+ * @n: number of bytes of s2.
+ * Return: NULL or pounter.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	int i = 0, j;
+
+	p = malloc(sizeof(char) * (strlen(s1) + n + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (; i < strlen(s1); i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		p[i] = s2[j];
+	}
+
+	p[i] = '\0';
+
+	return (p);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,1 +1,1 @@
-Learning to code in C programming language
+We learn more about malloc and free. we learn by solving 4 mandatory tasks and 2 advanced tasks
