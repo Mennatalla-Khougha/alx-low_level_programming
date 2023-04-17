303e0ec56c6cdd7a323b9e6dcb8d34976990725a
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-17 12:57:16 +0200

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+We learn about struct and typedef. We learn be solving 6 tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Description.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+*/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
