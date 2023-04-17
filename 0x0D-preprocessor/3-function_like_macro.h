c07f7ce379686df23bb9261d53845f9c87a4226d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-17 16:14:54 +0200

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _funcion_LIKE_MACRO_H_
+#ifndef _function_LIKE_MACRO_H_
 #define _function_LIKE_MACRO_H_
-#define ABS X (X > 0 ? X : X*-1)
+#define ABS (X) ((X) < 0 ? (X) * -1 : (X))
 #endif /* _function_LIKE_MACRO_H_ */
