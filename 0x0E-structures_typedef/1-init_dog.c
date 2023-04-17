9f0deceddb53e2fb1827eeab0baadd3247d80ac1
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-17 13:14:00 +0200

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+ * init_dog - initialize a struct dog variables.
+ * @d: the struct to be inialized.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *Return: nothing.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
