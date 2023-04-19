035599419c6f3816dbc551b0a3e660cd02139772
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-19 13:27:57 +0200

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - print a name using function call.
+ * @name: the name to be passed to the function.
+ * @f: the function to be called.
+ * Return: nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))t main(void)
+{
+	f(name);
+}
