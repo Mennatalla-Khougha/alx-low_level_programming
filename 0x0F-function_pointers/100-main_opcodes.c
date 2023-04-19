89539d337289a04df8550a0051d429c34314c8e0
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-19 23:36:38 +0200

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the opcodes of it's own main function
+ * @argc: number of arguments
+ * @argv: array of arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char **argv)
+{
+	int x, i;
+	int (*opcode)(int, char **) = main;
+	unsigned char print;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	x = atoi(argv[1]);
+	if (x < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < x; i++)
+	{
+		print = *opcode;
+		printf("%.2x", print);
+		printf(" ");
+		print++;
+	}
+	printf("\n");
+	return (0);
+}
