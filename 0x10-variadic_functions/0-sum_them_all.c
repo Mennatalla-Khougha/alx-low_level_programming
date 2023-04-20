42b2a85432be6b3bd0868164104f3a395dd2cd12
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-20 11:09:27 +0200

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,16 +10,16 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int result;
+	va_list args;
 
 	if (n == 0)
 		return (0);
 
-	va_list args;
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		result += va_arg(args, n);
+		result += va_arg(args, unsigned int);
 	}
 
 	va_end(args);
