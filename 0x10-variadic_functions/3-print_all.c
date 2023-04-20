fa4c304b1e1b02275212d1120e59dafd743c72de
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-20 23:52:12 +0200

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,33 +19,30 @@ void print_all(const char * const format, ...)
 	strcpy(str, format);
 	va_start(args, format);
 	while (str && *str)
+	{
+		switch (*str)
 		{
-			switch (*str)
-			{
-				case('c'):
-					printf("%c", va_arg(args, int));
-					break;
-				case('i'):
-					printf("%d", va_arg(args, int));
-					break;
-				case('f'):
-					printf("%f", va_arg(args, double));
-					break;
-				case('s'):
-					s = va_arg(args, char *);
-					if (s == NULL)
-					{
-						printf("(nil)");
-						break;
-					}
-					printf("%s", s);
-					break;
-			}
-			str++;
-			if (str == NULL)
-			{
+			case('c'):
+				printf("%c", va_arg(args, int));
 				break;
-			}
+			case('i'):
+				printf("%d", va_arg(args, int));
+				break;
+			case('f'):
+				printf("%f", va_arg(args, double));
+				break;
+			case('s'):
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+		}
+		str++;
+		if (*str)
 			printf(", ");
 		}
 	printf("\n");
