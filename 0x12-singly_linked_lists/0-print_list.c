cff54d838606e0baf07156688458272c6a2a78c7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-27 18:56:34 +0200

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list to print it's elements.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	while (h->next != NULL)
+	{
+		if (h == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+We learn about Singly linked lists. We do this by solving 7 tasks 5 mandatory tasks and 2 advance tasks
