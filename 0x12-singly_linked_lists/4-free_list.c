0708388b28c659ebc92464810a0ba685c9146757
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-27 21:25:53 +0200

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list.
+ * @head: pointer to the first element in the list.
+ * Return: nothing.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node = head, *node_free;
+
+	while (!node)
+	{
+		node_free = node;
+		node = node->next;
+		free(node_free);
+	}
+}
