d68852bed979e89070c4bd984c5ca0fb57b5ee20
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-04-30 01:00:20 +0200

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(node);
 		return (1);
 	}
-	while (i < index -1)
+	while (i < index - 1)
 	{
 		if (node->next == NULL)
 			return (-1);
