fdd1ea79c5919b61b27d1cb6422ed4ac706264c7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-05-02 15:06:03 +0200

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,48 @@
 #include "lists.h"
 
 /**
+ * floyd_loop - check if the list has a loop in it.
+ * @head: the linked list.
+ * Return: 0 or number of nodes.
+ */
+
+size_t floyd_loop(const listint_t *head)
+{
+	const listint_t *ptr_s = head, *ptr_f = head;
+	size_t i;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	ptr_s = ptr_s->next;
+	ptr_f = ptr_s->next;
+	i = 1;
+	while (ptr_f)
+	{
+		if (ptr_s == ptr_f)
+		{
+			ptr_s = head;
+			while (ptr_s != ptr_f)
+			{
+				i++;
+				ptr_s = ptr_s->next;
+				ptr_f = ptr_f->next;
+			}
+			ptr_s = ptr_s->next;
+			while (ptr_s != ptr_f)
+			{
+				i++;
+				ptr_s = ptr_s->next;
+			}
+			return (i);
+		}
+		ptr_s = ptr_s->next;
+		ptr_f = ptr_f->next->next;
+	}
+	return (0);
+}
+
+
+/**
  * print_listint_safe - print the element of a linked list.
  * @head: the first element of the list.
  * Return: number of elements of the lists.
@@ -8,23 +50,31 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
-	long int num;
-	const listint_t *node;
+	size_t i = 0, loops;
 
+	loops = floyd_loop(head);
 	if (head == NULL || head->next == NULL)
 		exit(98);
-	while (head)
+
+	if (loops == 0)
 	{
-		node = head->next;
-		num = head - node;
-		i++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		if (num < 0)
+		while (head)
 		{
-			printf("->[%p] %d\n", (void *)head, head->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			i++;
+			head = head->next;
+		}
+	}
+	else
+	{
+		while (i < loops)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			i++;
+		}
+
+		printf("->[%p] %d\n", (void *)head, head->n);
 		}
 	}
 	return (i);
