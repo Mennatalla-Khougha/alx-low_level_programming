a4873b231447a90abc45277a86facb0c9105995d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-05-02 15:43:18 +0200

    task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - free a list containing aloop.
+ * @head: pointer to the start of the list.
+ * Return: number of nodes freed.
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *node;
+	size_t i, loops;
+
+	loops = floyd_loop(*h);
+	if (loops == 0)
+	{
+		while (h & *h)
+		{
+			node = (*h)->next;
+			free(*h);
+			*h = node;
+			i++;
+		}
+	}
+	else
+	{
+		while (i < loops)
+		{
+			node = (*h)->next;
+			free(*h);
+			*h = node;
+			i++;
+		}
+		*h = NULL;
+	}
+	h = NULL;
+	return (i);
+}
