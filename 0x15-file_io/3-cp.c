07e64e47cee5c7c042a83650887766c817a79110
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-05-08 22:52:38 +0200

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,7 +40,11 @@ int main(int argc, char **argv)
 	ssize_t n_read, n_write;
 	char *buffer;
 
-	check_input(argc);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	buffer = malloc(1024);
 	if (buffer == NULL)
 	{
@@ -48,18 +52,17 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 	file_f = open(argv[1], O_RDONLY);
-	n_read = read(file_f, buffer, 1024);
-	if (file_f == -1 || n_read == -1)
+	if (file_f == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
 		exit(98);
 	}
 	file_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (n_read > 0)
+	while ((n_read = read(file_f, buffer, 1024)) > 0)
 	{
 		n_write = write(file_2, buffer, n_read);
-		if (n_write == -1 || file_2 == -1)
+		if (n_write == n_read || file_2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
