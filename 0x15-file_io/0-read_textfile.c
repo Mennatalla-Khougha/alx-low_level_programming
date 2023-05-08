b31a3c0a7d2ac6f9e9f286b6ea5e25ad75500be1
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-05-08 15:09:03 +0200

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+* read_textfile - read a text file and print it to the standard output.
+* @filename: the name of the file to be read.
+* @letters: the number of letters it should read and print.
+* Return: the number of letters it could read and print.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	char *buffer;
+	ssize_t n_read, n_write;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(file);
+		return (0);
+	}
+	n_read = read(file, buffer, letters);
+	if (n_read == -1)
+	{
+		close(file);
+		free(buffer);
+		return (0);
+	}
+	n_write = write(1, buffer, n_read);
+	if (n_write == -1)
+	{
+		close(file);
+		free(buffer);
+		return (0);
+	}
+	close(file);
+	free(buffer);
+	return (n_write);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+We learn about file I/O in c using open, close, read, write, dprintf functions, We learn by solving 5 tasks 4 mandatory and 1 advanced task
