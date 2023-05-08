91e223a64375afe6d6aa70fb616923b366d90fea
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-05-08 22:26:22 +0200

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,33 +29,37 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = malloc(1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	file_f = open(argv[1], O_RDONLY);
-	if (file_f == -1)
+	n_read = read(file_f, buffer, 1024);
+	if (file_f == -1 || n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
 		exit(98);
 	}
 	file_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
 		exit(99);
 	}
-	buffer = malloc(1024);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while ((n_read = read(file_f, buffer, 1024)) > 0)
+	while (n_read > 0)
 	{
 		n_write = write(file_2, buffer, n_read);
 		if (n_write == -1 || file_2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
 	}
