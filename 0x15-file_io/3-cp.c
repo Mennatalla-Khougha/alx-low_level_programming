9e2dfa45a73da1db310406bd9c5d0f6452229e15
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-05-08 22:15:50 +0200

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * close_file - close the file.
+ * @file: file to be closed.
+ */
+
+void close_file(int file)
+{
+	if (close(file) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
+}
+
+/**
  * main - copies the content of a file to another file.
  * @argc: number of argument.
  * @argv: array of array of argument.
@@ -11,7 +25,7 @@ int main(int argc, char **argv)
 {
 	int file_2, file_f;
 	ssize_t n_read, n_write;
-	char buffer[1024];
+	char *buffer;
 
 	if (argc != 3)
 	{
@@ -25,6 +39,17 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 	file_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	buffer = malloc(1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	while ((n_read = read(file_f, buffer, 1024)) > 0)
 	{
 		n_write = write(file_2, buffer, n_read);
@@ -34,16 +59,8 @@ int main(int argc, char **argv)
 			exit(99);
 		}
 	}
-	if (close(file_f) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", file_f);
-		exit(100);
-	}
-	if (close(file_2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", file_2);
-		exit(100);
-	}
+	close_file(file_f);
+	close_file(file_2);
 	free(buffer);
 	return (0);
 }
