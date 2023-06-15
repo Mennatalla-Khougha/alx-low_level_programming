20c7c613865f6ff7c5105082d223fefbdee9e74a
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-15 20:04:29 +0300

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode = create_node(n), *current;
+	dlistint_t *newNode = create_node(n), *current = *h;
 	unsigned int i = 0;
 
 	if (newNode == NULL)
