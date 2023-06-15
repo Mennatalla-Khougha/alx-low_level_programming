220bb748b2a592cc5f4e2f27236c5793c0af5e26
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-15 21:04:54 +0300

    task 10

---
+++ 0x17-doubly_linked_lists/102-palindrome.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+result = 0
+
+for i in range(100, 1000):
+    for j in range(i, 1000):
+            product = i * j;
+            if is_palindrome(product) and product > result:
+                result = product
+print(f"{result}")
