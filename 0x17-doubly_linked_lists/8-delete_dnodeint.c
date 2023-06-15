6b535d34e2fab0a0e64c2311e004efc59e086d8e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-15 20:35:45 +0300

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete a node at specific index
+ * @head: linked list
+ * @index: index to delete the linked list
+ * Return: 1 success, -1 fail
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *current = *head;
+
+	if (index == 0)
+	{
+		if (current != NULL)
+		{
+			*head = current->next;
+			if (*head != NULL)
+				(*head)->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
+	while (current)
+	{
+		if (i == index)
+		{
+			current->next->prev = current->prev;
+			current->prev->next = current->next;
+			free(current);
+			return (1);
+		}
+		current = current->next;
+		i++;
+	}
+	return (-1);
+}
