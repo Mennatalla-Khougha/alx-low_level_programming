68550c6b9a3aad8decc425ef24636d788e3e98cf
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-20 00:51:07 +0300

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.github.com/Mennatalla-Khougha/alx-low_level_programming/master/0x18-dynamic_libraries/libfunc.so
-export LD_PRELOAD=/$PWD/libfunc.os
+wget -P /tmp/ https://raw.github.com/Mennatalla-Khougha/alx-low_level_programming/master/0x18-dynamic_libraries/libfunc.so
+export LD_PRELOAD=/tmp/libfunc.os
