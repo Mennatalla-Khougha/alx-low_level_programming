76f2985738a0c847bd9cb49ea5a3d4cf7db06c9d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-07-06 18:33:17 +0300

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (table == NULL)
 	{
@@ -19,6 +20,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		free(table);
 		return (NULL);
-		}
+	}
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
 	return (table);
 }
