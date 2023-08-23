8e4e2b449491e871c4e4daad4ec96b8b41f49575
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-24 00:33:04 +0300

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,15 +6,17 @@
 def island_perimeter(grid):
     """calculate the perimeter of the island"""
     prem = 0
+    width = len(grid[0]) - 1
+    height = len(grid) - 1
     for i, row in enumerate(grid):
         for j, ele in enumerate(row):
             if ele:
-                if grid[i][j+1] == 0:
+                if j == width or grid[i][j+1] == 0:
                     prem += 1
-                if grid[i][j-1] == 0:
+                if j == 0 or grid[i][j-1] == 0:
                     prem += 1
-                if grid[i+1][j] == 0:
+                if i == height or grid[i+1][j] == 0:
                     prem += 1
-                if grid[i-1][j] == 0:
+                if i == 0 or grid[i-1][j] == 0:
                     prem += 1
     return prem
