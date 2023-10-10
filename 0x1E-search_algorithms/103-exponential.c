b91225fef97c99d2886c17c28f3d9130e842c864
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-10 14:07:56 +0300

    task 10

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,5 +1,21 @@
 #include <stddef.h>
 #include <stdio.h>
+/**
+ * print_array - print the array being searched
+ * @array:  pointer to the first element of the array to search in
+ * @size: number of elements in array
+*/
+void print_array(int *array, int size)
+{
+	int i;
+
+	printf("Searching in array: %d", array[0]);
+	for (i = 1; i < size; i++)
+	{
+		printf(", %d", array[i]);
+	}
+	printf("\n");
+}
 
 /**
  * exponential_search - searches for a value in an array of integers
@@ -8,4 +24,30 @@
  * @value:  the value to search for
  * Return: the index of the value or -1.
 */
-int exponential_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t prop = 1, left, right, mid;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	for (prop = 1; prop < size; prop *= 2)
+	{
+		if (value <= array[prop])
+			break;
+		printf("Value checked array[%ld] = [%d]\n", prop, array[prop]);
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n",  prop / 2, prop - 1);
+	left = prop / 2, right = prop - 1;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		print_array((array + left), (right - left) + 1);
+		if (value == array[mid])
+			return (mid);
+		else if (value < array[mid])
+			right = mid - 1;
+		else if (value > array[mid])
+			left = mid + 1;
+	}
+	return (-1);
+}
