fd5d6a9c827731e2979e4b4c21be1f4e6fde038e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-10 21:56:31 +0300

    task 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,32 +12,38 @@
 */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	int i, j, jump = sqrt(size), flag = 0;
-	listint_t *head, *prev;
+	int jump = sqrt((int)size), i, j;
+	listint_t *tmp = list, *next = list;
 
-	if (!list)
+	if (list == NULL || size == 0 || value < list->n)
 		return (NULL);
-	for (i = jump, prev = list, head = list; head->next; i += jump)
+	for (i = jump; i < (int)size; i += jump)
 	{
-		for (j = head->index;
-			j < (int)size && head->next && (int)head->index < i;
-			j++, head = head->next)
-			;
-		printf("Value checked at index [%ld] = [%d]\n", head->index, head->n);
-		if ((head->n > value || i >= (int)size) && !flag)
-		{
-			if (i >= (int)size)
-				i = size - 1;
-			printf("Value found between indexes [%ld] and [%d]\n", prev->index, i);
-			size = i + 1;
-			head = prev;
-			i = prev->index - 1;
-			jump = 1;
-			flag++;
-		}
-		else if (head->n == value)
-			return (head);
-		prev = head;
+		while ((int)tmp->index != i)
+			tmp = tmp->next;
+		if (tmp->index + 1 == size)
+			break;
+		printf("Value checked at index [%d] = [%d]\n", i, tmp->n);
+		if (value <= tmp->n)
+			break;
+	}
+	if (i == (int)size)
+	{
+		while (tmp->index != size - 1)
+			tmp = tmp->next;
+		printf("Value checked at index [%d] = [%d]\n", (int)size - 1, tmp->n);
+		printf("Value found between indexes [%d] and [%ld]\n",  i - jump, size - 1);
+	}
+	else
+		printf("Value found between indexes [%d] and [%d]\n",  i - jump, i);
+	while ((int)next->index != i - jump)
+		next = next->next;
+	for (j = i - jump; j < i && j < (int)size; j++)
+	{
+		printf("Value checked at index [%d] = [%d]\n", j, next->n);
+		if (value == next->n)
+			return (next);
+		next = next->next;
 	}
 	return (NULL);
 }
