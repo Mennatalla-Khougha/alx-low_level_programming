8da64932a025a17f65b798355090c964f6a2c340
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-10 21:04:44 +0300

    task 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,19 +12,18 @@
 */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	int jump, i, j;
+	int jump = sqrt((int)size), i, j;
 	listint_t *tmp = list;
 
 	if (list == NULL || size == 0)
 		return (NULL);
-	jump = sqrt((int)size);
 	for (i = jump; i < (int)size; i += jump)
 	{
 		while ((int)tmp->index != i)
 			tmp = tmp->next;
+		printf("Value checked at index [%d] = [%d]\n", i, tmp->n);
 		if (value <= tmp->n)
 			break;
-		printf("Value checked at index [%d] = [%d]\n", i, tmp->n);
 	}
 	if (i == (int)size)
 	{
@@ -34,10 +33,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		printf("Value found between indexes [%d] and [%ld]\n",  i - jump, size - 1);
 	}
 	else
-	{
-		printf("Value checked at index [%d] = [%d]\n", i, tmp->n);
 		printf("Value found between indexes [%d] and [%d]\n",  i - jump, i);
-	}
 	while ((int)list->index != i - jump)
 		list = list->next;
 	for (j = i - jump; j < i && j < (int)size; j++)
